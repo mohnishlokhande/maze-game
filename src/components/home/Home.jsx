e85490087358f686b48e9d07ad08360fc5dc7fc2
Mohnish Lokhande <mohnishlokhande14@gmail.com>
2025-01-02 15:56:51 +0530

    refactor home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,14 +1,12 @@
-// import { useEffect } from "react";
 import { homeBoard } from "../../utils/board";
-import { TILE_TYPES } from "../../utils/constants";
 import Tile from "../tile";
 import styles from "../board/Board.module.css";
 import styles2 from "./Home.module.css";
 import PropTypes from "prop-types";
-import { updateData } from "../../api/Api";
 import { useEffect, useRef, useState } from "react";
 import Character from "../character";
 import Editor from "../editor";
+import { handleKeyDownEvents } from "../../utils/helper";
 
 export default function Home(props) {
   const { players = [], myPlayer, setPage } = props;
@@ -20,71 +18,19 @@ export default function Home(props) {
   const [isTyping, setIsTyping] = useState(false);
   const [msg, setMsg] = useState("");
 
-  const alreadyOccupied = (x, y) => {
-    return players.some((p) => p.x === x && p.y === y);
-  };
-
-  const checkValidMove = (x, y) => {
-    if (x < 0 || x >= homeBoard[0].length || y < 0 || y >= homeBoard.length)
-      return 2;
-    else if (homeBoard[y][x] === TILE_TYPES.WATER) return 0;
-    else if (alreadyOccupied(x, y)) return 0;
-    else return 1;
-  };
-
-  const onEnter = () => {
-    if (checkValidMove(x, y) === 2) return;
-    if (homeBoard[y][x] === TILE_TYPES.GAME) {
-      updateData(id, { page: "forest" });
-      setPage("forest");
-    } else if (homeBoard[y][x] === TILE_TYPES.FREE_WAY) {
-      updateData(id, { page: "freeway" });
-      setPage("freeway");
-    } else if (homeBoard[y][x] === TILE_TYPES.SELL_APPLE) {
-      updateData(id, { score: 0 });
-    } else if (checkValidMove(x, y) === 1 || checkValidMove(x, y) === 0) {
-      boardRef?.current?.blur();
-      setIsTyping(true);
-      updateData(id, { isTyping: true });
-    }
-  };
-
   const handleKeyDown = (event) => {
-    event.preventDefault();
-    let updatedData = {};
-    switch (event.key) {
-      case "ArrowUp":
-        updatedData = { dir: 0, vector: (vector + 1) % 4 };
-        if (checkValidMove(x, y - 1)) updatedData.y = y - 1;
-        updateData(id, updatedData);
-        break;
-      case "ArrowRight":
-        updatedData = { dir: 1, vector: (vector + 1) % 4 };
-        if (checkValidMove(x + 1, y)) updatedData.x = x + 1;
-        updateData(id, updatedData);
-        break;
-      case "ArrowDown":
-        updatedData = { dir: 2, vector: (vector + 1) % 4 };
-        if (checkValidMove(x, y + 1)) updatedData.y = y + 1;
-        updateData(id, updatedData);
-        break;
-      case "ArrowLeft":
-        updatedData = { dir: 3, vector: (vector + 1) % 4 };
-        if (checkValidMove(x - 1, y)) updatedData.x = x - 1;
-        updateData(id, updatedData);
-        break;
-      case " ":
-        break;
-      case "Enter":
-        onEnter();
-        break;
-      case "Escape":
-        setMsg("");
-        updateData(id, { isTyping: false, msg: "" });
-        break;
-      default:
-        break;
-    }
+    handleKeyDownEvents(
+      id,
+      x,
+      y,
+      vector,
+      event,
+      setMsg,
+      setPage,
+      setIsTyping,
+      boardRef,
+      players
+    );
   };
 
   useEffect(() => {
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,3 +1,5 @@
+import { updateData } from "../api/Api";
+import { homeBoard } from "./board";
 import { TILE_DENSITIES, TILE_TYPES } from "./constants";
 
 export const generateRandomBoard = (rows, cols) => {
@@ -41,4 +43,87 @@ export const generateRandomBoard = (rows, cols) => {
   }
 
   return board;
+};
+
+const alreadyOccupied = (x, y, players) => {
+  console.log("event.key4", players);
+  return players?.some((p) => p.x === x && p.y === y);
+};
+
+const checkValidMove = (x, y, players) => {
+  console.log("event.key3", players);
+  if (x < 0 || x >= homeBoard[0].length || y < 0 || y >= homeBoard.length)
+    return 2;
+  else if (homeBoard[y][x] === TILE_TYPES.WATER) return 0;
+  else if (alreadyOccupied(x, y, players)) return 0;
+  else return 1;
+};
+
+const onEnter = (id, x, y, setPage, setIsTyping, players, boardRef) => {
+  console.log("event.key2", players);
+  let valid = checkValidMove(x, y, players);
+  if (valid === 2) return;
+  if (homeBoard[y][x] === TILE_TYPES.GAME) {
+    updateData(id, { page: "forest" });
+    setPage("forest");
+  } else if (homeBoard[y][x] === TILE_TYPES.FREE_WAY) {
+    updateData(id, { page: "freeway" });
+    setPage("freeway");
+  } else if (homeBoard[y][x] === TILE_TYPES.SELL_APPLE) {
+    updateData(id, { score: 0 });
+  } else if (valid === 1 || valid === 0) {
+    boardRef?.current?.blur();
+    setIsTyping(true);
+    updateData(id, { isTyping: true });
+  }
+};
+
+export const handleKeyDownEvents = (
+  id,
+  x,
+  y,
+  vector,
+  event,
+  setMsg,
+  setPage,
+  setIsTyping,
+  boardRef,
+  players
+) => {
+  event.preventDefault();
+  let updatedData = {};
+  console.log("event.key", players);
+  switch (event.key) {
+    case "ArrowUp":
+      updatedData = { dir: 0, vector: (vector + 1) % 4 };
+      if (checkValidMove(x, y - 1, players)) updatedData.y = y - 1;
+      updateData(id, updatedData);
+      break;
+    case "ArrowRight":
+      updatedData = { dir: 1, vector: (vector + 1) % 4 };
+      if (checkValidMove(x + 1, y, players)) updatedData.x = x + 1;
+      updateData(id, updatedData);
+      break;
+    case "ArrowDown":
+      updatedData = { dir: 2, vector: (vector + 1) % 4 };
+      if (checkValidMove(x, y + 1, players)) updatedData.y = y + 1;
+      updateData(id, updatedData);
+      break;
+    case "ArrowLeft":
+      updatedData = { dir: 3, vector: (vector + 1) % 4 };
+      if (checkValidMove(x - 1, y, players)) updatedData.x = x - 1;
+      updateData(id, updatedData);
+      break;
+    case " ":
+      break;
+    case "Enter":
+      onEnter(id, x, y, setPage, setIsTyping, players, boardRef);
+      break;
+    case "Escape":
+      setMsg("");
+      updateData(id, { isTyping: false, msg: "" });
+      break;
+    default:
+      break;
+  }
 };
